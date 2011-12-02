ec1eb747f806ea3f2ef10090939f4816b97ae1aa
Kevin <kevin@colorz.fr>
2011-12-02 12:24:17 +0100

    First commit

---
+++ inc/install.php
@@ -0,0 +1,26 @@
+<?php
+
+$chemin_site = explode('/',$_SERVER['PHP_SELF']);
+array_shift($chemin_site);
+array_pop($chemin_site);
+$chemin_site = implode('/',$chemin_site);
+$ndd = $_SERVER['SERVER_NAME'];
+
+file_put_contents($config_file,"<?php\n".
+"define('PL_DIR', '".TPL_DIR."');\n".
+"define('PL_URL', 'http://".$ndd.'/'.$chemin_site.'/'."');\n");
+
+// Re-Creation du htaccess
+file_put_contents('.htaccess',"
+Options +FollowSymlinks
+RewriteEngine on
+RewriteRule ^([0-9]+)$   /".$chemin_site."/index.php?width=$1&height=$1 [L]
+RewriteRule ^([0-9]+)/([0-9]+)$   /".$chemin_site."/index.php?width=$1&height=$2 [L]
+RewriteRule ^([0-9]+)/([0-9]+)/new$   /".$chemin_site."/index.php?width=$1&height=$2&new [L]");
+
+// Création du dossier cache
+@mkdir(TPL_DIR.'cache/');
+@chmod(TPL_DIR.'cache/',0755);
+
+echo 'install ok. plz reload';
+die;
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+$config_file = dirname(__FILE__).'/config.php';
+if(file_exists($config_file))
+	include $config_file;
+else {
+	
+	define('TPL_DIR',dirname(__FILE__).'/');
+
+	include TPL_DIR.'inc/install.php';
+
+
+}
+
+
+// Si aucune valeur, page de présentation.
+if(!isset($_GET['width'])){
+	echo '<div><img src="'.PL_URL.'300/200" alt="" /><br /><input type="text" value="'.PL_URL.'300/200" style="width:300px;" /></div>';
+	exit();
+}
+
+// On recupere les valeurs depuis GET
+$width = (isset($_GET['width']) && ctype_digit($_GET['width'])) ? $_GET['width'] : 100;
+$height = (isset($_GET['height']) && ctype_digit($_GET['height'])) ? $_GET['height'] : $width;
+$filename = 'cache/'.$width.'-'.$height.'.jpg';
+
+// Si un cache existe et est demandé
+if(file_exists(PL_DIR.$filename) && !isset($_GET['new'])){
+	header('location:'.PL_URL.$filename);
+	exit();
+}
+
+// On génère l'image de base
+$image = imagecreatetruecolor($width, $height);
+
+// On genere l'image temporaire
+$images_temp = glob(PL_DIR.'images/*.jpg');
+
+if(empty($images_temp)){
+	exit('Aucune image pour travailler.');
+}
+$img_temp = $images_temp[rand(0, count($images_temp)-1)];
+$image_temp = imagecreatefromjpeg($img_temp); 
+
+
+// On redimensionne l'image temporaire
+$image_temp_x = imagesx($image_temp);
+$image_temp_y = imagesy($image_temp);
+
+$rapport_image_temp = $image_temp_x / $image_temp_y;
+$rapport_base = $width / $height;
+
+$width_redim = $width;
+$height_redim = $height;
+
+$dst_x = 0;
+$dst_y = 0;
+
+if($rapport_image_temp > $rapport_base){
+	$height_redim = $height;
+	$width_redim = $rapport_image_temp*$height;
+	$dst_x = ($width-$width_redim)/2;
+}
+else if($rapport_image_temp < $rapport_base){
+	$width_redim = $width;
+	$height_redim = $width / $rapport_image_temp;
+	$dst_y = ($height-$height_redim)/2;
+}
+imagecopyresampled($image, $image_temp, $dst_x, $dst_y, 0, 0, $width_redim, $height_redim, $image_temp_x, $image_temp_y);
+
+// Le fichier est au format JPG
+header ("Content-type: image/jpg");
+
+// On genere l'image finale
+imagejpeg($image,PL_DIR.$filename);
+imagejpeg($image);
+
