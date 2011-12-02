34381d8b381c7c3b7efcb64d7de8a94d53201608
Kevin <kevin@colorz.fr>
2011-12-02 13:56:37 +0100

    Debug Install racine

--- inc/install.php
+++ inc/install.php
@@ -1,22 +1,23 @@
 <?php
-
-$chemin_site = explode('/',$_SERVER['PHP_SELF']);
-array_shift($chemin_site);
-array_pop($chemin_site);
-$chemin_site = implode('/',$chemin_site);
-$ndd = $_SERVER['SERVER_NAME'];
+$chemin_site = '';
+$chemin_site_tmp = explode('/',$_SERVER['PHP_SELF']);
+if(count($chemin_site_tmp) > 2){
+	array_shift($chemin_site_tmp);
+	array_pop($chemin_site_tmp);
+	$chemin_site = implode('/',$chemin_site_tmp).'/';
+	$ndd = $_SERVER['SERVER_NAME'];
+}
 
 file_put_contents($config_file,"<?php\n".
 "define('PL_DIR', '".TPL_DIR."');\n".
-"define('PL_URL', 'http://".$ndd.'/'.$chemin_site.'/'."');\n");
-
+"define('PL_URL', 'http://".$ndd.'/'.$chemin_site."');\n");
 // Re-Creation du htaccess
 file_put_contents('.htaccess',"
 Options +FollowSymlinks
 RewriteEngine on
-RewriteRule ^([0-9]+)$   /".$chemin_site."/index.php?width=$1&height=$1 [L]
-RewriteRule ^([0-9]+)/([0-9]+)$   /".$chemin_site."/index.php?width=$1&height=$2 [L]
-RewriteRule ^([0-9]+)/([0-9]+)/new$   /".$chemin_site."/index.php?width=$1&height=$2&new [L]");
+RewriteRule ^([0-9]+)$   /".$chemin_site."index.php?width=$1&height=$1 [L]
+RewriteRule ^([0-9]+)/([0-9]+)$   /".$chemin_site."index.php?width=$1&height=$2 [L]
+RewriteRule ^([0-9]+)/([0-9]+)/new$   /".$chemin_site."index.php?width=$1&height=$2&new [L]");
 
 // Création du dossier cache
 @mkdir(TPL_DIR.'cache/');
@@ -26,5 +27,5 @@ RewriteRule ^([0-9]+)/([0-9]+)/new$   /".$chemin_site."/index.php?width=$1&heigh
 @mkdir(TPL_DIR.'images/');
 @chmod(TPL_DIR.'images/',0755);
 
-echo 'install ok. plz reload';
+echo '<script>location.reload(true);</script>';
 die;
