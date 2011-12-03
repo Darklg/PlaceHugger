12a73b5699926980e639197baadefc041b2f1915
Darklg <kev.rocher@gmail.com>
2011-12-03 12:32:02 +0100

    Division du fichier index.php + validation HTML home + Changement noms de fichiers

---
+++ inc/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="fr-FR">
+    <head>
+	<meta charset="UTF-8" />
+	<title><?php echo PL_SITENAME; ?></title>
+    </head>
+    <body>
+		<div>
+			<img src="<?php echo PL_URL; ?>300x200" alt="" /><br />
+			<input type="text" value="<?php echo PL_URL; ?>300x200" style="width:300px;" />
+		</div>
+    </body>
+</html>
---
+++ inc/main-config.php
@@ -0,0 +1,5 @@
+<?php
+
+if(!defined('PL_SITENAME')){
+	define('PL_SITENAME','PlaceHugger');
+}
