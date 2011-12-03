689f165d3d274d274449199d5c11e640a7a7434c
Darklg <kev.rocher@gmail.com>
2011-12-03 13:16:39 +0100

    Nouveau message d'erreur en l'absence d'images dans images/

--- inc/main-config.php
+++ inc/main-config.php
@@ -8,5 +8,5 @@ if(!defined('PL_SITENAME')){
 // On récupère les images utilisables
 $images_temp = glob(PL_DIR.'images/*.jpg');
 if(empty($images_temp)){
-	exit('Aucune image pour travailler.');
+	exit('Ajoutez des images dans images/ pour continuer ;)');
 }
